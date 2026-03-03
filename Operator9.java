8f2cf6bd0b1549f13ac2f9f2a7616a211787c75c
Karishini She/her <karishinim@gmail.com>
2026-03-03 13:30:11 +0530

    Add files via upload

---
+++ Operator9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Operator9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int usernameMatch = sc.nextInt();  
+        int passwordMatch = sc.nextInt();  
+        int accountLocked = sc.nextInt();  
+        if (usernameMatch == 1 && passwordMatch == 1 && !(accountLocked == 1)) {
+            System.out.println("Login Success");
+        } else {
+            System.out.println("Login Failed");
+        }
+        sc.close();
+    }
+}
