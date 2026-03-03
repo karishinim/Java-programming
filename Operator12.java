69e564e8435c5e204e02c229b730ef1b354dcfa6
Karishini She/her <karishinim@gmail.com>
2026-03-03 13:55:52 +0530

    Add files via upload

---
+++ Operator12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Operator12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int fileSize = sc.nextInt();
+        int compressionCycles = sc.nextInt();
+
+        int compressedSize = fileSize >> compressionCycles;
+
+        System.out.println("Compressed Size = " + compressedSize);
+
+        sc.close();
+    }
+}
