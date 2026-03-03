2c716cc5368b4e8e006e232747dcdf722bcc6955
Karishini She/her <karishinim@gmail.com>
2026-03-03 11:54:58 +0530

    Add files via upload

---
+++ LoanApproval.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LoanApproval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int salary = sc.nextInt();
+        int creditScore = sc.nextInt();
+        if (salary > 25000 && creditScore > 700) {
+            System.out.println("Loan Approved");
+        } else {
+            System.out.println("Loan Rejected");
+        }
+        sc.close();
+    }
+}
