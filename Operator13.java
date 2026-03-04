72128690b9be7ec6f6b2732e42ea985aec7981b4
Karishini She/her <karishinim@gmail.com>
2026-03-04 09:13:44 +0530

    Add files via upload

---
+++ Operator13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Operator13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int connectionValue = sc.nextInt();
+        int applianceMask = sc.nextInt();
+
+        if ((connectionValue & applianceMask) != 0) {
+            System.out.println("Appliance Active");
+        } else {
+            System.out.println("Inactive");
+        }
+
+        sc.close();
+    }
+}
