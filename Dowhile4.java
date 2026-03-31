8b1bb264900117513e2fa8c580b7c5d653a5ba7e
Karishini She/her <karishinim@gmail.com>
2026-03-31 20:55:31 +0530

    Add files via upload

---
+++ Dowhile4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dowhile4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long result = 1;   
+        int counter = 1;
+        while (counter <= n) {
+            result = result * counter;
+            counter++;
+        }
+        System.out.println("Factorial of " + n + " = " + result);
+        sc.close();
+    }
+}
