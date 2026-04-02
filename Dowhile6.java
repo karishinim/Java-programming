b87838a8963027a0ddfa5edfe4a5ec9e5eac0c49
Karishini She/her <karishinim@gmail.com>
2026-04-02 22:21:06 +0530

    Add files via upload

---
+++ Dowhile6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Dowhile6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int original = number;
+        int reversed = 0;
+        while (number > 0) {
+            int digit = number % 10;     
+            reversed = reversed * 10 + digit; 
+            number = number / 10;
+        }
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+        sc.close();
+    }
+}
